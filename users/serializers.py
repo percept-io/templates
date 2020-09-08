9face8cf24ca36348f331e1716203c5070634bef
Bernardo Laing <bernardo.laing@gmail.com>
2020-09-07 19:46:12 -0500

    Add ID to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,7 @@ from .models import CustomUser
 class UserFullSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'is_staff', 'is_active', 'date_joined']
+        fields = ['id', 'email', 'is_staff', 'is_active', 'date_joined']
 
 
 class UserPasswordSerializer(serializers.ModelSerializer):
